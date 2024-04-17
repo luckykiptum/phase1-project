3f1da6fcce4acbac5f8b276b3f54d2a097f3ce81
lucky <luckykiptum@gmail.com>
2024-04-17 11:22:36 +0300

    done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
     getBeverages();
 });
 
 function getBeverages() {
-    let beverageList = document.getElementById('theList');
+    const menuList = document.getElementById('theList');
+    const shoppingList = document.getElementById('cart');
 
     fetch("https://coffee-json.onrender.com/coffee")
         .then(res => res.json())
@@ -13,15 +16,28 @@ function getBeverages() {
                 let listItem = document.createElement('li');
                 listItem.innerText = beverage.id;
 
-                listItem.addEventListener('click', () => {
+                listItem.onclick = () => {
+                    addItemToShoppingList(beverage);
                     displayDetails(beverage);
-                });
-
-                beverageList.appendChild(listItem);
-
-                console.log(beverage.id);
+                };
+                menuList.appendChild(listItem);
             });
-        });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+
+    const addItemToShoppingList = beverage => {
+        const newItem = document.createElement('li');
+        newItem.textContent = beverage.id;
+        shoppingList.appendChild(newItem);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.onclick = () => {
+            shoppingList.removeChild(newItem);
+    };
+
+    newItem.appendChild(deleteButton);
+    shoppingList.appendChild(newItem);
 }
 
 function displayDetails(beverage) {
@@ -35,9 +51,9 @@ function displayDetails(beverage) {
     coffeeName.innerText = beverage.id;
     type.innerText = beverage.type;
     origin.innerText = beverage.origin;
-    organic.innerText = beverage.organic ? 'Yes' : 'No'; 
+    organic.innerText = beverage.organic ? 'Yes' : 'No';
     roast.innerText = beverage.roast;
     price.innerText = beverage.price;
 }
-
+}
 
